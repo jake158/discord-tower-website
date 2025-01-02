69747e039b00a2a6a0ce2719cb42c3b17c184097
jake158 <kulikovpavel2801@gmail.com>
2025-01-02 03:53:58 -0700

    Add navbar, react routing, basic layout

--- TowerWebsite.Client/src/App.tsx
+++ TowerWebsite.Client/src/App.tsx
@@ -1,10 +1,27 @@
-import { Button } from '@/components/ui/button';
+import { Grid, GridItem } from '@chakra-ui/react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import WelcomePage from './pages/WelcomePage';
+import TosPage from './pages/TosPage';
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
   return (
-    <>
-      <Button colorPalette="teal">Button</Button>
-    </>
+    <Router>
+      <Grid templateRows="auto 1fr" height="100vh">
+        <GridItem>
+          <NavBar />
+        </GridItem>
+        <GridItem bg="teal">
+          <Routes>
+            <Route path="/" element={<WelcomePage />} />
+            <Route path="/welcome" element={<WelcomePage />} />
+            <Route path="/tos" element={<TosPage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+          </Routes>
+        </GridItem>
+      </Grid>
+    </Router>
   );
 }
 
---
+++ TowerWebsite.Client/src/components/NavBar.tsx
@@ -0,0 +1,59 @@
+import {
+  HStack,
+  Image,
+  Text,
+  Tabs,
+  Box,
+  SegmentGroupValueChangeDetails,
+} from '@chakra-ui/react';
+import { LuSun, LuShieldCheck, LuLayoutDashboard } from 'react-icons/lu';
+import { useLocation, useNavigate } from 'react-router-dom';
+import TowerIcon from '../assets/react.svg';
+
+const NavBar = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const normalizedPath =
+    location.pathname === '/' ? '/welcome' : location.pathname;
+
+  const handleTabChanged = (details: SegmentGroupValueChangeDetails) => {
+    navigate(details.value);
+  };
+
+  return (
+    <HStack w="100%" justify="space-between" p={4}>
+      <HStack flex="1">
+        <Image src={TowerIcon} boxSize="15" />
+        <Text>Tower</Text>
+      </HStack>
+
+      <Box position="absolute" left="50%" transform="translateX(-50%)">
+        <Tabs.Root
+          value={normalizedPath}
+          onValueChange={handleTabChanged}
+          variant="line"
+        >
+          <Tabs.List bg="bg.muted" rounded="l3" p="1">
+            <Tabs.Trigger value="/welcome">
+              <LuSun />
+              Welcome
+            </Tabs.Trigger>
+            <Tabs.Trigger value="/tos">
+              <LuShieldCheck />
+              TOS
+            </Tabs.Trigger>
+            <Tabs.Trigger value="/dashboard">
+              <LuLayoutDashboard />
+              Dashboard
+            </Tabs.Trigger>
+            <Tabs.Indicator rounded="l2" />
+          </Tabs.List>
+        </Tabs.Root>
+      </Box>
+
+      <Box flex="1" />
+    </HStack>
+  );
+};
+
+export default NavBar;
